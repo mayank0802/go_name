60873b3619e4ff5a4f648d56834c0669f55e38a2
mayank0802 <mayankdixitkv214@gmail.com>
2021-12-17 07:44:19 +0530

    first commit

---
+++ controller/controller.go
@@ -0,0 +1,62 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	//".../config"
+	//"../model"
+	"github.com/mayank0802/go_name/config"
+
+	"github.com/mayank0802/go__name/model"
+)
+
+// git token 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
+func getStudent(w http.ResponseWriter, r *http.Request) {
+	var students model.Student
+	var arrStudents []model.Student
+
+	db := config.Connect()
+	defer db.close()
+	row, err := db.Query("SELECT E_NO, NAME FROM Students")
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	for row.Next() {
+		err = row.Scan(&students.ID, &students.Name)
+		if err != nil {
+			log.Fatal(err.Error())
+		} else {
+			arrStudents = append(arrStudents, students)
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(arrStudents)
+
+}
+
+//creating a new student
+func createStudent(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+	defer db.Close()
+	err := r.ParseMultiplepartForm(4096)
+	if err != nil {
+		panic(err)
+	}
+	name := r.FormValue("name")
+	class := r.FormValue("class")
+	subject := r.FormValue("student")
+	age := r.FormValue("age")
+
+	_, err = db.Exec("INSERT INTO STUDENTS (NAME, AGE, SUBJECT, CLASS)VALUES(?, ?,?,?)", name, age, subject, class)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode("insert successfully")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mayank0802/go_name
+
+go 1.13
